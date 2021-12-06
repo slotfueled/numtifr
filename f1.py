ccc3b11df51e65f55092dfdc1636ae50c8fd4f7d
shivansh <shivansh13iitkgp@gmail.com>
2021-12-06 18:09:24 +0530

    2ndwha

---
+++ f1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def Square(a):
+    return(a**2)
+a= float(input('Enter number: '))
+print('Square of the number = ',Square(a))
+
+b=np.array([1,3,5])
+print("Square of a list =",Square(b))
---
+++ f2.py
@@ -0,0 +1,6 @@
+import f3
+import numpy as np
+
+a= float(input('Enter and number: '))
+print ('Square of the number = ',f3.Square(a))
+
